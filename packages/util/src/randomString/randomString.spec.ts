6aed58ab10db1ec0a2ccd4badc11e27505bd0cc8
pratamaizzat <pratama.izzat231298@gmail.com>
2023-01-19 23:00:42 +0700

    fix(util): randomString method
    randomString not supported crypto so major change on randomString method

--- packages/util/src/randomString/randomString.spec.ts
+++ packages/util/src/randomString/randomString.spec.ts
@@ -2,11 +2,9 @@ import randomString from './randomString'
 
 describe('randomString function', () => {
   it('should return a random string', () => {
-    const rand1 = randomString()
-    const rand2 = randomString(5)
-    const rand3 = randomString(5, 'ascii')
-
-    console.log({ rand2, rand3 })
-    expect(rand1).toEqual(expect.any(String))
+    expect(randomString()).toEqual(expect.any(String))
+    expect(randomString()).toHaveLength(5)
+    expect(randomString(10)).toHaveLength(10)
+    expect(randomString(26)).toHaveLength(26)
   })
 })
--- packages/util/src/randomString/randomString.ts
+++ packages/util/src/randomString/randomString.ts
@@ -1,5 +1,7 @@
-import * as cryto from 'node:crypto'
-
-export default function randomString(length = 5, encoding: BufferEncoding = 'hex') {
-  return cryto.randomBytes(length).toString(encoding)
+export default function randomString(length = 5, string = ''): string {
+  string += Math.random().toString(20).substring(2, length)
+  if (string.length > length) {
+    return string.slice(0, length)
+  }
+  return randomString(length, string)
 }
