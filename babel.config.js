541e1402b071438e270c2c44cf7b62744eb61956
pratamaizzat <pratama.izzat231298@gmail.com>
2023-01-21 16:18:03 +0700

    perf(root): import change

--- babel.config.js
+++ babel.config.js
@@ -42,10 +42,10 @@ module.exports = function getBabelConfig(api) {
         namespace: 'IA',
       },
     ],
-    ['@babel/plugin-proposal-class-properties', { loose: true }],
-    ['@babel/plugin-proposal-private-methods', { loose: true }],
-    ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
-    ['@babel/plugin-proposal-object-rest-spread', { loose: true }],
+    // ['@babel/plugin-proposal-class-properties', { loose: true }],
+    // ['@babel/plugin-proposal-private-methods', { loose: true }],
+    // ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
+    // ['@babel/plugin-proposal-object-rest-spread', { loose: true }],
     [
       '@babel/plugin-transform-runtime',
       {
