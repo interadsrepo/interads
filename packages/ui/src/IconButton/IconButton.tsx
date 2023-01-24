e04e2c385ce7e1d06d522ad956fab8efc5f7d1b2
pratamaizzat <pratama.izzat231298@gmail.com>
2023-01-24 14:39:58 +0700

    fix(ui): ref error on IconButton component

--- packages/ui/src/IconButton/IconButton.tsx
+++ packages/ui/src/IconButton/IconButton.tsx
@@ -3,7 +3,10 @@ import * as React from 'react'
 import type { IconButtonProps } from './props'
 import IAIconButton from './css'
 
-const IconButton: React.FC<IconButtonProps> = function IconButton(props: IconButtonProps) {
+const IconButton = React.forwardRef<HTMLButtonElement, IconButtonProps>(function IconButton(
+  props,
+  ref,
+) {
   const {
     palette = 'secondary',
     size = 'md',
@@ -13,6 +16,7 @@ const IconButton: React.FC<IconButtonProps> = function IconButton(props: IconBut
     className,
     ...rest
   } = props
+
   return (
     <IAIconButton
       className={`IAIconButton-root ${!className ? '' : className}`.trim()}
@@ -21,10 +25,11 @@ const IconButton: React.FC<IconButtonProps> = function IconButton(props: IconBut
       fullWidth={fullWidth}
       variant={variant}
       {...rest}
+      ref={ref}
     >
       {children}
     </IAIconButton>
   )
-}
+})
 
 export default IconButton
