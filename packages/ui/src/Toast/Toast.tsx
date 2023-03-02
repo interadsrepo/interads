539c4892bc984d705c5d301ed13200ddbd5596b2
pratamaizzat <pratama.izzat231298@gmail.com>
2023-03-02 16:39:41 +0700

    feat(ui): expandable description toast component

--- packages/ui/src/Toast/Toast.tsx
+++ packages/ui/src/Toast/Toast.tsx
@@ -2,9 +2,11 @@ import * as React from 'react'
 import { useToast } from '../hook'
 import { CheckCircle, Question, WarningCircle } from '../icon'
 import IAToast from './css'
+import { ToastItemProps } from './props'
 
-const Toast: React.FC = function Toast() {
+function ToastItem({ toast }: { toast: ToastItemProps }) {
   const [pause, setPause] = React.useState(false)
+  const [expand, setExpand] = React.useState(false)
   const {
     toast: { toasts },
     dispatch,
@@ -28,25 +30,45 @@ const Toast: React.FC = function Toast() {
     }
   }, [toasts, dispatch, pause])
 
+  const toggleExpand = () => {
+    setExpand((prev) => !prev)
+  }
+
   return (
-    <IAToast className="toast-container">
-      {toasts.map((toast) => (
-        <div
-          onMouseEnter={() => setPause(true)}
-          onMouseLeave={() => setPause(false)}
-          key={toast.id}
-          className={`toast-notification toast ${toast.type} `}
-        >
-          <span className="toast-icon">
-            {toast.type === 'success' && <CheckCircle weight="fill" className="icon" />}
-            {toast.type === 'error' && <WarningCircle weight="fill" className="icon" />}
-            {toast.type === 'info' && <Question weight="fill" className="icon" />}
-          </span>
-          <div className="toast-information">
-            <h3 className="text text-toast-title">{toast.title}</h3>
-            <p className="text text-toast-desc">{toast.description}</p>
-          </div>
-          {/* <button
+    <React.Fragment key={toast.id}>
+      <div
+        onMouseEnter={() => setPause(true)}
+        onMouseLeave={() => setPause(false)}
+        className={`toast-notification toast ${toast.type} ${expand ? 'open' : ''}`}
+      >
+        <span className="toast-icon">
+          {toast.type === 'success' && <CheckCircle weight="fill" className="icon" />}
+          {toast.type === 'error' && <WarningCircle weight="fill" className="icon" />}
+          {toast.type === 'info' && <Question weight="fill" className="icon" />}
+        </span>
+        <div className="toast-information">
+          <h3 className="text text-toast-title">{toast.title}</h3>
+          <p className="text text-toast-desc">{toast.description}</p>
+        </div>
+        <div className="toast-action">
+          <button onClick={toggleExpand} type="button" className="button btn-toast">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="icon icon-tabler icon-tabler-caret-down"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              fill="none"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M18 15l-6 -6l-6 6h12" transform="rotate(180 12 12)" />
+            </svg>
+          </button>
+          <button
             type="button"
             className="button btn-toast"
             onClick={() =>
@@ -56,8 +78,40 @@ const Toast: React.FC = function Toast() {
               })
             }
           >
-          </button> */}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="icon icon-tabler icon-tabler-x"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              fill="none"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <line x1="18" y1="6" x2="6" y2="18" />
+              <line x1="6" y1="6" x2="18" y2="18" />
+            </svg>
+          </button>
         </div>
+      </div>
+      <div className={`toast-expand ${toast.type} ${expand ? 'open' : ''}`.trim()}>
+        <p className="text textExpand">{toast.description}</p>
+      </div>
+    </React.Fragment>
+  )
+}
+const Toast: React.FC = function Toast() {
+  const {
+    toast: { toasts },
+  } = useToast()
+
+  return (
+    <IAToast className="toast-container">
+      {toasts.map((toast) => (
+        <ToastItem key={toast.id} toast={toast} />
       ))}
     </IAToast>
   )
--- packages/ui/src/Toast/props.ts
+++ packages/ui/src/Toast/props.ts
@@ -1,1 +1,7 @@
 export type ToastProps = {}
+export type ToastItemProps = {
+  id: number
+  title: string
+  description: string
+  type: 'success' | 'error' | 'info'
+}
